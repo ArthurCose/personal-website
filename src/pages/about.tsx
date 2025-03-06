18e0b4982c3128220e3384083dfbc2915b001814
Arthur Cosentino <arthurcose@gmail.com>
2025-03-06 16:30:44 -0500

    Update about text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,22 +4,16 @@ export default function About() {
       <h2>Hi, I'm Arthur</h2>
 
       <p>
-        I am a Software Engineer in the EST time zone with professional
-        experience in C++, JavaScript + TypeScript, React-based frameworks, and
-        MongoDB.
+        I am a Software Engineer with professional experience in C++,
+        TypeScript, React-based frameworks, and MongoDB. I also frequently use
+        Rust, Lua, SQL, and have minor experience with PHP and Python.
       </p>
       <br />
 
       <p>
-        Outside of professional experience, I frequently use Rust, Lua, SQL, and
-        have minor experience with PHP and Python.
-      </p>
-      <br />
-
-      <p>
-        In my free time I work on a few projects for various online communities,
-        but I've excluded links to these projects to separate my personal and
-        online life.
+        In my free time, I work on a few projects for various online
+        communities, but I've excluded links to these projects to separate my
+        personal and online lives.
       </p>
     </>
   );
