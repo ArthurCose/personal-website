a9ad4320836d03fbc59dee60792ae7d2616d393d
Arthur Cosentino <arthurcose@gmail.com>
2025-10-18 22:21:34 -0400

    Reduce rerenders in gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { MouseEventHandler, useState } from "react";
 import styles from "@/styles/Gallery.module.css";
 import GalleryFullscreen from "@/components/gallery_fullscreen";
@@ -7,6 +8,32 @@ type Props = {
   renderListItem: (i: number, onClick: MouseEventHandler) => React.ReactNode;
   renderFullscreenItem: (i: number) => React.ReactNode;
 };
+
+type ListProps = {
+  setIndex: (i: number) => void;
+  setInitialBounds: (bounds: DOMRect) => void;
+  totalItems: Props["totalItems"];
+  renderItem: Props["renderListItem"];
+};
+
+const GalleryList = React.memo(function ({
+  setIndex,
+  setInitialBounds,
+  totalItems,
+  renderItem: renderItem,
+}: ListProps) {
+  return (
+    <div className={styles.gallery}>
+      {Array.from({ length: totalItems }, (_, i) =>
+        renderItem(i, (e) => {
+          const target = e.target as HTMLElement;
+          setInitialBounds(target.getBoundingClientRect());
+          setIndex(i);
+        })
+      )}
+    </div>
+  );
+});
 
 export default function Gallery({
   totalItems,
@@ -20,15 +47,12 @@ export default function Gallery({
 
   return (
     <>
-      <div className={styles.gallery}>
-        {Array.from({ length: totalItems }, (_, i) =>
-          renderListItem(i, (e) => {
-            const target = e.target as HTMLElement;
-            setInitialBounds(target.getBoundingClientRect());
-            setIndex(i);
-          })
-        )}
-      </div>
+      <GalleryList
+        setIndex={setIndex}
+        setInitialBounds={setInitialBounds}
+        totalItems={totalItems}
+        renderItem={renderListItem}
+      />
 
       <GalleryFullscreen
         initialBounds={initialBounds}
